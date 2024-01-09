71d5c1c2fbe97cf6e445940fc9eae615918389cb
Lasbleic <lasbleis.corentin@gmail.com>
2024-01-09 02:12:20 +0100

    Initializing projects and solution

---
+++ WaterGridSimulator/WaterGridSimulator.cpp
@@ -0,0 +1,11 @@
+// WaterGridSimulator.cpp : Définit les fonctions de la bibliothèque statique.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "WaterGridSimulator.h"
+
+// TODO: Il s'agit d'un exemple de fonction de bibliothèque
+void fnWaterGridSimulator()
+{
+}
