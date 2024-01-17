6343952a67f2ea863ac0dc50e7fd572ded0820e0
lasbleic <lasbleis.corentin@gmail.com>
2024-01-17 14:29:24 +0100

    Add Random number generator to Test projects

---
+++ WaterGridSimulatorTests/Random.cpp
@@ -0,0 +1,42 @@
+#include "pch.h"
+#include <random>
+#include "Random.hpp"
+
+// Handling randomness
+std::mt19937 rng;
+std::uniform_int_distribution<int> uniform_int_dist{ 0, 100 };
+std::uniform_int_distribution<int> uniform_int_dist_below_5{ 0, 5 };
+std::uniform_int_distribution<int> uniform_int_dist_above_5{ 5, 100 };
+std::uniform_real_distribution<double> uniform_double_dist{ 0, 100 };
+std::uniform_real_distribution<double> uniform_double_dist_below_5{ 0, 5 };
+std::uniform_real_distribution<double> uniform_double_dist_above_5{ 5, 100 };
+
+int getRandomInt() 
+{ 
+	return uniform_int_dist(rng); 
+}
+
+int getRandomIntBelow5() 
+{
+	return uniform_int_dist_below_5(rng);
+}
+
+int getRandomIntAbove5()
+{
+	return uniform_int_dist_above_5(rng);
+}
+
+double getRandomDouble() 
+{ 
+	return uniform_double_dist(rng); 
+}
+
+double getRandomDoubleBelow5()
+{
+	return uniform_double_dist_below_5(rng);
+}
+
+double getRandomDoubleAbove5()
+{
+	return uniform_double_dist_above_5(rng);
+}
---
+++ WaterGridSimulatorTests/Random.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+int getRandomInt();
+
+int getRandomIntBelow5();
+
+int getRandomIntAbove5();
+
+double getRandomDouble();
+
+double getRandomDoubleBelow5();
+
+double getRandomDoubleAbove5();
