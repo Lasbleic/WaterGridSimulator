f0a52d34c704f2854ece699f6e0c841d44510963
Lasbleic <lasbleis.corentin@gmail.com>
2024-01-19 01:49:46 +0100

    Adding Pond tests file

---
+++ WaterGridSimulatorTests/PondTests.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../WaterGridSimulator/Cell.hpp"
+#include "Random.hpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace WaterGridSimulatorTests
+{
+	TEST_CLASS(PondTests)
+	{
+	public:
+		
+		TEST_METHOD(TestGetPond)
+		{
+			
+		}
+	};
+}
