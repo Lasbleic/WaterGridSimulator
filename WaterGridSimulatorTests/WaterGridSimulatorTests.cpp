cf2a2708aa1f7aec996cac0aa650fe6cab325f79
lasbleic <lasbleis.corentin@gmail.com>
2024-01-19 18:59:27 +0100

    Add file for WaterGridSimulatorTests

---
+++ WaterGridSimulatorTests/WaterGridSimulatorTests.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace WaterGridSimulatorTests
+{
+	TEST_CLASS(WaterGridSimulatorTests)
+	{
+	public:
+
+		TEST_METHOD(TestAddFloor)
+		{
+
+		}
+
+	};
+}
